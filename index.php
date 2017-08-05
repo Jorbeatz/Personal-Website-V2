ae73eba76252f2dcf8dcfa2f48919349af6036c8
Jordy Guntur <jguntur@u.rochester.edu>
2017-08-05 13:04:18 -0400

    baby steps

---
+++ index.php
@@ -0,0 +1,250 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Jordy Guntur | Web Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/iconsmind.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/interface-icons.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+    </head>
+    <body class="scroll-assist">
+        <a id="top"></a>
+        <div class="loader"></div>
+        <!--end of modal-container-->
+        <div class="main-container transition--fade">
+            <section class="imagebg height-90 parallax" data-overlay="6">
+                <div class="background-image-holder">
+                    <img alt="pic" src="http://via.placeholder.com/1600x1069" />
+                </div>
+                <div class="container pos-vertical-center">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2">
+                            <p class="lead">Hi, I’m Jordy, I’m a full-stack developer with experience building SEO-optimized websites and web applications. My most recent project was a custom website and content management system for SportStrata, a New York City based Sport Psychology firm.</p>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="masonry-contained section--overlap">
+                <div class="container">
+                    <div class="row">
+                        <div class="masonry box-shadow-wide">
+                            <div class="masonry__filters masonry__filters--outside text-center" data-filter-all-text="Show All"></div>
+                            <div class="masonry__container masonry--gapless masonry--animate">
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="SportStrata" src="img/port-ss-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>SportStrata</h5>
+                                                    <span>
+                                                        <em>Website</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="Guntur Cabinetry" src="img/port-gc-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>Guntur Cabinetry</h5>
+                                                    <span>
+                                                        <em>Website</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="Lucy Hutner" src="img/port-lh-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>Lucy Hutner (In Progress)</h5>
+                                                    <span>
+                                                        <em>Personal Website</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="Union Square Practice" src="img/port-rl-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>Union Square Practice</h5>
+                                                    <span>
+                                                        <em>Landing Page</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="SportStrata" src="img/port-sl-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>SportStrata</h5>
+                                                    <span>
+                                                        <em>Landing Page</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="design">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="SongBox" src="img/port-sb-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>SongBox</h5>
+                                                    <span>
+                                                        <em>App Design</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="HiveMind" src="img/port-hm-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>HiveMind</h5>
+                                                    <span>
+                                                        <em>Web Application</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 masonry__item" data-masonry-filter="websites">
+                                    <a href="#">
+                                        <div class="hover-element hover-element-1" data-title-position="top,right">
+                                            <div class="hover-element__initial">
+                                                <img alt="UofR" src="img/port-sa-1.png" />
+                                            </div>
+                                            <div class="hover-element__reveal" data-overlay="9">
+                                                <div class="boxed">
+                                                    <h5>University of Rochester</h5>
+                                                    <span>
+                                                        <em>Website</em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end hover element-->
+                                    </a>
+                                </div>
+                            </div>
+                            <!--end masonry container-->
+                        </div>
+                    </div>
+                    <!--end row-->
+                </div>
+                <!--end container-->
+            </section>
+            <section class="height-50 section--overlap bg--dark">
+                <div class="container pos-vertical-center">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text-center">
+                            <p class="lead">Interested in working with me on an upcoming project?</p>
+                            <a class="btn btn--primary" href="#">
+                                <span class="btn__text">
+                                    Let's Talk
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <footer class="footer-3 bg--white text-center-xs">
+                <div class="col-sm-3">
+                    <img alt="logo" class="logo" src="img/logo-dark.png" />
+                    <!-- <span>(347) 891 7632</span> -->
+                    <a href="mailto:jguntur@u.rochester.edu" class="">
+                        jguntur@u.rochester.edu
+                    </a>
+                </div>
+                <div class="col-sm-6 text-center">
+                </div>
+                <div class="col-sm-3 text-right text-center-xs">
+                    <ul class="social-list">
+                        <li>
+                            <a href="#">
+                                <i class="socicon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="socicon-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <span class="type--fine-print">&copy; Copyright
+                        <span class="update-year">2017</span> Jordy Guntur - All Rights Reserved</span>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/easypiechart.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scrollreveal.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
